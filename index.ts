b6e042bb81db6a6e7d4502efa27ec5dde95a5bbc
Edward Smale <essmale2005@gmail.com>
2022-05-29 12:30:14 +0100

    First commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { AllowEndgamePlugin } from "./src/plugin";
+
+export * from "./src";
+export default AllowEndgamePlugin;
---
+++ src/plugin.ts
@@ -0,0 +1,20 @@
+import {
+    HindenburgPlugin,
+    WorkerPlugin,
+    MessageHandler,
+    EndGameMessage,
+    PacketContext,
+    MessageDirection
+} from "@skeldjs/hindenburg";
+
+@HindenburgPlugin("hbplugin-allow-endgame", "1.0.0", "none")
+export class AllowEndgamePlugin extends WorkerPlugin {
+    @MessageHandler(EndGameMessage, { override: true })
+    onEndGameMessage(message: EndGameMessage, { sender }: PacketContext) {
+        const player = sender.getPlayer();
+        if (!player)
+            return;
+
+        sender.room?.decoder.emitDecoded(message, MessageDirection.Serverbound, player);
+    }
+}
